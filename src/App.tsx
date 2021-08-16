4a3d25807665db254102b1e4be8a8502f568d116
Chintana Meegamarachchi <chintana@appsolve.com.au>
2021-08-17 00:08:59 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Card, CardContent} from '@material-ui/core';
+import {Button} from 'devextreme-react';
+
+function App() {
+  return (
+    <div className="App">
+      <Card>
+        <CardContent>
+          <Button>DevExtreme button</Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default App;
